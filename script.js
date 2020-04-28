5c91cb0cf191e4b233ee086c8530f6b50cb46e7d
onesoftwareengineer <48726358+onesoftwareengineer@users.noreply.github.com>
2020-04-28 12:46:03 +0300

    this commit will add notifications for testing purposes

--- script.js
+++ script.js
@@ -33,6 +33,8 @@ button.addEventListener('click', (e) => {
     } 
 } );
 
+notifyMe();
+
 startWork = () => {
     //used to calculate time passed between two setintervals
     startWorkTime = new Date();
@@ -85,3 +87,29 @@ showReward = () => {
 }
 
 enterPause();
+
+function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Hi there!");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+  }
